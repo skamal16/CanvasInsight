d1521ac0931848446b8330acca46da68dc3cddfa
Syed Anas Kamal <syedanaskamal@gmail.com>
2020-06-15 21:53:39 +0500

    Added Android Application

---
+++ Paint-master1/app/src/main/java/com/example/paint/MainActivity.java
@@ -0,0 +1,348 @@
+package com.example.paint;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.paint.R;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import yuku.ambilwarna.AmbilWarnaDialog;
+
+public class MainActivity extends AppCompatActivity {
+
+    static PaintView paintView;
+    private int defaultColor;
+    private int STORAGE_PERMISSION_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Button button;
+        Button undo;
+        Button save;
+        Button redo;
+        Button clear;
+        Button Result;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        paintView = findViewById(R.id.paintView);
+        button = findViewById(R.id.change_color_button);
+        undo = findViewById(R.id.undo_button);
+        redo = findViewById(R.id.redo_button);
+        save = findViewById(R.id.save_button);
+        clear = findViewById(R.id.clear_button);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        final TextView textView = findViewById(R.id.current_pen_size);
+
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        paintView.initialise(displayMetrics);
+
+        textView.setText("Pen size: " + seekBar.getProgress());
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                openColourPicker();
+
+            }
+
+        });
+        clear.setOnClickListener(new View.OnClickListener() {
+
+                                     @Override
+                                     public void onClick(View v) {
+
+                                         paintView.clear();
+
+                                     }
+
+                                 }
+        );
+        redo.setOnClickListener(new View.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(View v) {
+
+                                        paintView.redo();
+
+                                    }
+
+                                }
+        );
+        undo.setOnClickListener(new View.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(View v) {
+
+                                        paintView.undo();
+
+                                    }
+
+                                }
+        );
+        save.setOnClickListener(new View.OnClickListener() {
+
+                                    @Override
+                                    public void onClick(View v) {
+
+                                        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                                            requestStoragePermission();
+                                        }
+                                        paintView.saveImage();
+                                        connectServer();
+                                    }
+                                }
+        );
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                paintView.setStrokeWidth(seekBar.getProgress());
+                textView.setText("Pen size: " + seekBar.getProgress());
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    public void connectServer() {
+
+
+        //use 10.0.2.2:5000 if using an emulator
+        //use wireless lan ipv4 of server if using device on same network
+        String postUrl = "http://192.168.10.12:5000/application_request";
+
+        MultipartBody.Builder multipartBodyBuilder = new MultipartBody.Builder().setType(MultipartBody.FORM);
+
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inPreferredConfig = Bitmap.Config.RGB_565;
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        try {
+            // Read BitMap by file path.
+            Toast.makeText(this, paintView.image.getAbsolutePath(), Toast.LENGTH_LONG).show();
+            Bitmap bitmap = BitmapFactory.decodeFile(paintView.image.getAbsolutePath(), options);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        } catch (Exception e) {
+
+            return;
+        }
+        byte[] byteArray = stream.toByteArray();
+
+        multipartBodyBuilder.addFormDataPart("image", "Android_Flask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray));
+
+
+        RequestBody postBodyImage = multipartBodyBuilder.build();
+
+//        RequestBody postBodyImage = new MultipartBody.Builder()
+//                .setType(MultipartBody.FORM)
+//                .addFormDataPart("image", "androidFlask.jpg", RequestBody.create(MediaType.parse("image/*jpg"), byteArray))
+//                .build();
+
+        postRequest(postUrl, postBodyImage);
+    }
+
+    void postRequest(String postUrl, RequestBody postBody) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(postUrl)
+                .post(postBody)
+                .build();
+
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                // Cancel the post on failure.
+                call.cancel();
+
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+            }
+
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                // In order to access the TextView inside the UI thread, the code is executed inside runOnUiThread()
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    private void requestStoragePermission() {
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission needed")
+                    .setMessage("Needed to save image")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            dialog.dismiss();
+
+                        }
+
+                    })
+                    .create().show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                Toast.makeText(this, "Access granted", Toast.LENGTH_LONG).show();
+
+            } else {
+
+                Toast.makeText(this, "Access denied", Toast.LENGTH_LONG).show();
+
+            }
+
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        switch (item.getItemId()) {
+
+            case R.id.clear_button:
+                paintView.clear();
+                return true;
+            case R.id.undo_button:
+                paintView.undo();
+                return true;
+            case R.id.redo_button:
+                paintView.redo();
+                return true;
+            case R.id.save_button:
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+                    requestStoragePermission();
+
+                }
+                paintView.saveImage();
+                return true;
+
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    private void openColourPicker() {
+
+        AmbilWarnaDialog ambilWarnaDialog = new AmbilWarnaDialog(this, defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+                Toast.makeText(MainActivity.this, "Unavailable", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+
+                defaultColor = color;
+
+                paintView.setColor(color);
+
+            }
+
+        });
+
+        ambilWarnaDialog.show(); // add
+
+    }
+}
